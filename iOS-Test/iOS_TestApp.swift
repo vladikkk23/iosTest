67ff1ca30257e72cf2a6cda764702378e08e203b
Vladislav Movileanu <vladislav.movileanu@turnit.com>
2023-08-24 15:58:19 +0300

    Initial Commit

---
+++ iOS-Test/iOS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TestApp.swift
+//  iOS-Test
+//
+//  Created by Introduct on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
